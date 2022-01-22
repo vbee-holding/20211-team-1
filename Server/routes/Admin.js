56c64454d5126433baea95e098383f310cbff433
uyencdm20176063 <uyen.cdm176063@sis.hust.edu.vn>
2022-01-22 14:57:14 +0700

    add crawl data

--- Server/routes/Admin.js
+++ Server/routes/Admin.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const AdminController = require ('../controller/AdminController');
+const express = require("express");
+const AdminController = require("../controller/AdminController");
 const router = express.Router();
 
-router.get('/', AdminController.getAdmins);
-router.post('/', AdminController.postAdmin);
-router.get('/:adminId', AdminController.getAdmin);
-router.put('/:adminId', AdminController.putAdmin);
-router.delete('/:adminId', AdminController.deleteAdmin);
+router.get("/", AdminController.getAdmins);
+router.post("/", AdminController.postAdmin);
+
+router.get("/:adminId", AdminController.getAdmin);
+router.put("/:adminId", AdminController.putAdmin);
+router.delete("/:adminId", AdminController.deleteAdmin);
+
+router.post("/login", AdminController.login);
 
 module.exports = router;
--- Server/routes/Articles.js
+++ Server/routes/Articles.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const ArticleController = require ('../controller/ArticleController');
+const express = require("express");
+const ArticleController = require("../controller/ArticleController");
 const router = express.Router();
 
-router.get('/', ArticleController.getArticles);
-router.post('/', ArticleController.postArticle);
-router.get('/:articleId', ArticleController.getArticle);
-router.put('/:articleId', ArticleController.putArticle);
-router.delete('/:articleId', ArticleController.deleteArticle);
+router.get("/", ArticleController.getArticles);
+router.post("/", ArticleController.postArticle);
+router.get("/:articleId", ArticleController.getArticle);
+router.put("/:articleId", ArticleController.putArticle);
+router.delete("/:articleId", ArticleController.deleteArticle);
+// router.delete("/delete/all", ArticleController.deleteAllArticles);
 
 module.exports = router;
