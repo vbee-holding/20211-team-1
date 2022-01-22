da7de8ac3e6e67910f7dd83da82fdd3724b98565
HungTK2001 <hungtk28102001@gmail.com>
2022-01-22 17:09:32 +0700

    HungTK: Check api

--- client/src/services/User/HomeServices.js
+++ client/src/services/User/HomeServices.js
@@ -1,1 +1,45 @@
 // Dùng để xử lý logic ở đây, nó chỉ bao gồm các hàm 
+
+import { getArticleByIdAPI } from "../../apis/server-api/user-api/article";
+import { getSourceByIdAPI } from "../../apis/server-api/user-api/source-api";
+
+export async function getAllArticle() {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return null;
+    }
+}
+
+
+export async function getArticleById(id) {
+    try {
+        const articleResult = await getArticleByIdAPI(id);
+        if (articleResult.success) {
+            const article = articleResult.data;
+            const sourceResult = await getSourceByIdAPI(article.sourceId);
+            if(sourceResult.success){
+                const source = sourceResult.data;
+                return {
+                    thumbnail: article.thumbnail,
+                    link: article.link,
+                    title: article.title,
+                    timeShift: article.releaseTime,
+                    logoSource: source.logo,
+                }
+            }else{
+                console.log("Failed to fetch source");
+                return null;
+            }
+            
+        } else {
+            console.log("Failed to fetch article");
+            return null;
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
--- client/src/shares/components/MainNewsV3.js
+++ client/src/shares/components/MainNewsV3.js
@@ -1,18 +1,19 @@
-import MainNew from '../../assets/images/main-news.png'
-import Source from '../../assets/images/source.png'
-import Category from '../../assets/images/main-news-v3.png'
-export default function MainNewsV3(){
+
+export default function MainNewsV3({ data }) {
+    const { thumbnail, link, title, logoSource, timeShift } = data;
     return (
         <div className='relative'>
             <div className='overflow-hidden'>
-            <img src={Category} alt="" className='max-w-mi-5 max-h-mi-5  rounded' />
+                <img src={thumbnail} alt="" className='max-w-mi-5 max-h-mi-5 w-i-5 h-i-5 rounded' />
             </div>
             <div className='text-xl  mt-3'>
-                <span>Sáng 4/1, khai mạc Kỳ họp bất thường lần thứ Nhất, Quốc hội khóa XV </span>
+                <span>
+                    <a href={link}>{title}</a>
+                </span>
             </div>
             <div className=' bg-white flex gap-2 align-middle p-1 rounded-lg absolute top-0 left-0 mt-2 ml-2'>
-                <img src={Source} alt="" className='max-h-4' />
-                <span className='inline-block text-xs text-gray-400'>1 giờ</span>
+                <img src={logoSource} alt="" className='max-h-4' />
+                <span className='inline-block text-xs text-gray-400 truncate'>{timeShift} giờ</span>
             </div>
         </div>
     )
