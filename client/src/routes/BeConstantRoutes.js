b68057812f3cba36fd422d0ea71025dc2bef09cc
HungTK2001 <hungtk28102001@gmail.com>
2022-01-23 10:21:23 +0700

    HungTK: Modified database and create user controller

---
+++ client/src/routes/BeConstantRoutes.js
@@ -0,0 +1,33 @@
+const url = "http://localhost:4000/api/v1/user/articles/"
+export const BE_CATEGORY_CONSTANT_ROUTES = [
+    {
+        "url": url + "61ecba34f22c945535177975",
+        "name": "covid-19",
+    },
+    {
+        "url": url + "61ecba4ff22c945535177979",
+        "name": "entertainment",
+    },
+    {
+        "url": url + "61ecba5ef22c94553517797d",
+        "name": "sport",
+    },
+    {
+        "url": url + "61ecba67f22c945535177981",
+        "name": "technology",
+    },
+    {
+        "url": url + "61ecba77f22c945535177985",
+        "name": "video",
+    },
+    {
+        "_id": url + "61ecba81f22c945535177989",
+        "name": "vietnam-travel",
+        "__v": 0
+    },
+    {
+        "_id": url + "61ecba95f22c94553517798d",
+        "name": "world-travel",
+        "__v": 0
+    }
+]
