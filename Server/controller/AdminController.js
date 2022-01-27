3d4e2fdcdc383ed3b11cf18b26da6607b4c4f75c
HaiTD <hai.td194270@sis.hust.edu.vn>
2022-01-28 00:49:34 +0700

    privateAPI

--- Server/controller/AdminController.js
+++ Server/controller/AdminController.js
@@ -19,105 +19,262 @@ class AdminRouter {
     }
   }
 
-  async getAdmin(req, res, next) {
-    const { adminId } = req.params;
-    try {
-      const admin = await Admin.findById(adminId);
-      res.json({
-        success: true,
-        data: admin,
-      });
-    } catch (err) {
-      res.json({
-        success: false,
-        error: err,
-      });
+    constructor() {
+        this.refreshTokens = [];
     }
-  }
 
-  // Login
-  async login(req, res, next) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    try {
-      const admin = await Admin.findOne({ email: email });
-
-      if (admin) {
-        const match = await bcrypt.compare(password, admin.password);
-
-        if (match) {
-          const token = jwt.sign(
-            {
-              exp: Math.floor(Date.now() / 1000) + 60 * 60,
-              email: admin.email,
-            },
-            "secret"
-          );
-
-          res.json({
-            success: true,
-            data: {
-              access_token: token,
-              exp: Math.floor(Date.now() / 1000) + 60 * 60,
-            },
-          });
+    generateAccessToken (admin) {
+        try{
+            return jwt.sign({_id : admin._id, email : admin.email}, "mySecretKey", {expiresIn : "15m"});
         }
-      } else {
+        catch (err) {
+            console.log(err);
+        }
+    }
+ 
+    generateRefreshToken (admin) {
+        try{
+            return jwt.sign({_id : admin._id, email : admin.email}, "myRefreshSecretKey", {expiresIn : "30d"});
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    logIn = async (req, res, next) => {
+        const { email, password } = req.body;
+        try {
+            const admin =  await Admin.find({
+                email : email,
+                password : password
+            });
+            if(admin.length >= 1) {
+                const accessToken = this.generateAccessToken(admin);
+                const refreshToken = this.generateRefreshToken(admin);
+ 
+                this.refreshTokens.push(refreshToken);
+                res.json({
+                    success : true,
+                    data : {
+                        accessToken : accessToken,
+                        refreshToken : refreshToken,
+                    },
+                });
+            }
+            else {
+                res.json({
+                    success : false,
+                    error : "email or password incorrect",
+                });
+            }
+        }
+        catch (err) {
+            console.log(err);
+            res.status(400).json({
+                success: false,
+                error : err,
+            })
+        }
+
+    }
+
+    refreshToken = async (req, res, next) => {
+        try {
+            const { refreshToken } = req.body;
+            if(!refreshToken) return res.status(401).json({
+                success : false,
+                data : {
+                    message : "you are not authenticated ",
+                }
+            });
+            
+            if(!this.refreshTokens.includes(refreshToken)) return res.status(403).json({
+                success : false,
+                data : {
+                    message : "Refresh token is not valid",
+                },
+            });
+            
+            jwt.verify(refreshToken, "myRefreshSecretKey", (err, admin) => {
+                if(err) {
+                    res.status(400).json({
+                        success: false,
+                        error : err,
+                    })
+                }
+            
+                this.refreshTokens = this.refreshTokens.filter((token) => token !== refreshToken);
+                const newRefreshToken = this.generateRefreshToken(admin);
+                const newAccessToken = this.generateAccessToken(admin);
+                this.refreshTokens.push(newRefreshToken);
+        
+                res.json({
+                    success : true,
+                    data : {
+                        accessToken : newAccessToken,
+                        refreshToken : newRefreshToken,
+                    }
+                })
+            })
+        }
+        catch (err) {
+            console.log(err);
+            res.status(400).json({
+                success : false,
+                err : err,
+            })
+        }
+    }
+
+    
+    logOut = async (req, res, next) => {
+        const { refreshToken } = req.body;
+        this.refreshTokens = this.refreshTokens.filter(token => token !== refreshToken);
         res.json({
-          success: false,
-          message: "Tài khoản không tồn tại hoặc sai mật khẩu!",
+            success : true,
+            data : {
+                message : "You loged out succesfully"
+            },
         });
-      }
-    } catch (err) {
-      res.json({
-        success: false,
-        error: err,
-      });
     }
-  }
 
-  // SignUp
-  async postAdmin(req, res, next) {
-    var admin = req.body;
+    putAdmin = async (req, res, next) => {
+        const newAdmin = req.body;
+        const { adminId } = req.params;
+        try {
+            await Admin.findByIdAndUpdate( adminId , newAdmin);
+            res.json({
+                success : true,
+                data : newAdmin,
+            })
+        }
+        catch (err) {
+            res.status(400).json({
+                success: false,
+                error : err,
+            })
+        }
 
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-
-    admin.password = bcrypt.hashSync(admin.password, salt);
-
-    try {
-      await Admin.create(admin);
-
-      res.json({
-        success: true,
-        data: admin,
-      });
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        error: err,
-      });
-      console.log(err);
     }
-  }
 
-  async putAdmin(req, res, next) {
-    const newAdmin = req.body;
-    const { adminId } = req.params;
-    try {
-      await Admin.findByIdAndUpdate(adminId, newAdmin);
-      res.json({
-        success: true,
-        data: newAdmin,
-      });
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        error: err,
-      });
+    postAdmin = async (req, res, next) => {
+        const newAdmin = req.body;
+        try {
+            await Admin.create(newAdmin);
+            res.json({
+                success : true,
+                data : newAdmin,
+            })
+        }
+        catch (err) {
+            res.status(400).json({
+                success: false,
+                error : err,
+            })
+        }
+
     }
-  }
+
+  // async getAdmin(req, res, next) {
+  //   const { adminId } = req.params;
+  //   try {
+  //     const admin = await Admin.findById(adminId);
+  //     res.json({
+  //       success: true,
+  //       data: admin,
+  //     });
+  //   } catch (err) {
+  //     res.json({
+  //       success: false,
+  //       error: err,
+  //     });
+  //   }
+  // }
+
+  // // Login
+  // async login(req, res, next) {
+  //   const email = req.body.email;
+  //   const password = req.body.password;
+
+  //   try {
+  //     const admin = await Admin.findOne({ email: email });
+
+  //     if (admin) {
+  //       const match = await bcrypt.compare(password, admin.password);
+
+  //       if (match) {
+  //         const token = jwt.sign(
+  //           {
+  //             exp: Math.floor(Date.now() / 1000) + 60 * 60,
+  //             email: admin.email,
+  //           },
+  //           "secret"
+  //         );
+
+  //         res.json({
+  //           success: true,
+  //           data: {
+  //             access_token: token,
+  //             exp: Math.floor(Date.now() / 1000) + 60 * 60,
+  //           },
+  //         });
+  //       }
+  //     } else {
+  //       res.json({
+  //         success: false,
+  //         message: "Tài khoản không tồn tại hoặc sai mật khẩu!",
+  //       });
+  //     }
+  //   } catch (err) {
+  //     res.json({
+  //       success: false,
+  //       error: err,
+  //     });
+  //   }
+  // }
+
+  // // SignUp
+  // async postAdmin(req, res, next) {
+  //   var admin = req.body;
+
+  //   const saltRounds = 10;
+  //   const salt = bcrypt.genSaltSync(saltRounds);
+
+  //   admin.password = bcrypt.hashSync(admin.password, salt);
+
+  //   try {
+  //     await Admin.create(admin);
+
+  //     res.json({
+  //       success: true,
+  //       data: admin,
+  //     });
+  //   } catch (err) {
+  //     res.status(400).json({
+  //       success: false,
+  //       error: err,
+  //     });
+  //     console.log(err);
+  //   }
+  // }
+
+  // async putAdmin(req, res, next) {
+  //   const newAdmin = req.body;
+  //   const { adminId } = req.params;
+  //   try {
+  //     await Admin.findByIdAndUpdate(adminId, newAdmin);
+  //     res.json({
+  //       success: true,
+  //       data: newAdmin,
+  //     });
+  //   } catch (err) {
+  //     res.status(400).json({
+  //       success: false,
+  //       error: err,
+  //     });
+  //   }
+  // }
 
   async deleteAdmin(req, res, next) {
     const { adminId } = req.params;
--- Server/routes/Sources.js
+++ Server/routes/Sources.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const SourceController = require ('../controller/SourceController');
+const Verify = require ('../util/Verify');
 
 router.get('/', SourceController.getSources);
 router.get('/:sourceId', SourceController.getSource);
-router.post('/', SourceController.postSource);
-router.put('/:sourceId', SourceController.putSource);
-router.delete('/:sourceId', SourceController.deleteSource);
+router.post('/', Verify.verify, SourceController.postSource);
+router.put('/:sourceId', Verify.verify, SourceController.putSource);
+router.delete('/:sourceId', Verify.verify, SourceController.deleteSource);
 
 
 module.exports = router;
