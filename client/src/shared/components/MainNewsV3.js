6f777683f575e583d5a59f13971e3df2e31395f5
HungTK2001 <hungtk28102001@gmail.com>
2022-02-18 09:13:14 +0700

    HungTK: Add numOfViews to news

--- client/src/shared/components/MainNewsV3.js
+++ client/src/shared/components/MainNewsV3.js
@@ -25,7 +25,15 @@ export default function MainNewsV3({ data, reload }) {
                         <LazyLoad placeholder={<LoadingV2 />}>
                             <img src={source.logo} alt="" className='max-h-4 object-fill' />
                         </LazyLoad>
-                        <span className='text-xs font-medium font-text text-gray-400'>{convertTime(Date.now() - releaseTime)}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span className='text-xs font-semibold font-text text-gray-400'>{convertTime(Date.now() - releaseTime)}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                        <span className='text-xs block font-semibold font-text text-gray-400 -ml-1'>{numOfViews}</span>
                     </div>
                     <span className='text-xl sm:text-lg font-semibold font-text hover:text-blue-400'>
                         <a href={link} target="_blank" rel="noreferrer" onClick={
